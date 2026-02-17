0ac7e7a32bbb06b69b63e670460efe0af3bcbac2
JOHN DANE GALVEZ <nlzerogodix@gmail.com>
2026-02-17 12:11:28 +0800

    Delete func_count.php

--- func_count.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'connection.php';
-$sql = "SELECT COUNT(*) AS total FROM usertbl";
-$result = $conn->query($sql);
-$row = $result->fetch_assoc();
-
-echo "<h3>Output for COUNT() Function</h3>";
-echo "Total Users: " . $row['total'];
-?>
